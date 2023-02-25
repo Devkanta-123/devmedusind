de9a570471223c8b380d562438d426ab66b2d616
unknown <konsam dev>
2023-02-25 12:59:55 +0530

    Assignment

---
+++ Assignment 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Assignment
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+           // CountLetters cs = new CountLetters();
+            Student s = new Student();
+            s.studentDetails();
+            s.GetMarks();
+            s.DisplayResult();
+            s.GetDetails();
+            //cs.MyName();
+            //cs.GetString();
+          
+
+            Console.Read();
+        }
+
+
+
+    }
+
+}
