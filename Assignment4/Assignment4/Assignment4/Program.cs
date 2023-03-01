01156e4e76cf18c21d6b44c4e1a36ebcda8f0d6a
unknown <konsam dev>
2023-03-01 20:59:02 +0530

    push

---
+++ Assignment4/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("-----------------First Question-------------------");
+            // Saledetails s = new Saledetails(12, 34,0,0, Convert.ToDateTime("09/02/2023")); //
+            //s.Sales();
+            //// s.Showdata();
+            // Console.WriteLine("-----------------Second Question-------------------"); 
+           
+            Scholarship s = new Scholarship(200,45);
+            float scholarshipamount = s.Merit();
+            Console.WriteLine("Scholarship Amount:{0}", scholarshipamount);
+            //  Doctor d = new Doctor("Konsam Devkanta","Shillong","KIORTY", 1200, "NEUROLOGY");
+
+            Console.ReadLine();
+        }
+       
+    }
+    
+}
---
+++ Assignment4/Assignment4/Assignment4/Saledetails.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Saledetails
+    {
+        int Salesno;
+        int Productno;
+        int Price;
+       DateTime dateofsale;
+        int Qty;
+        private double TotalAmount;
+
+        public void Sales()
+        { 
+            unsafe {          //pointer
+
+                Price = 150;
+                Qty = 2;
+                fixed(int* ptr1 = &Price)
+                fixed(int* ptr2 = &Qty)
+               TotalAmount = Qty * Price;
+
+            }
+        
+        
+        }
+
+        public  Saledetails(int salesno,int productno, int qnty,int price, DateTime dateOFsale) //constructor
+        {
+            Salesno = salesno;
+            Productno = productno;
+            Qty = qnty;
+            Price = price;
+            dateofsale = dateOFsale;
+           
+
+
+          
+
+        }
+        public void Showdata()
+        {
+
+           
+            Console.WriteLine("********INVOICE********");
+            Console.WriteLine("Salesno: " + Salesno);
+            Console.WriteLine("Product No : " + Productno);
+            Console.WriteLine("Qty : " + Qty);
+            Console.WriteLine("Price : " + Price);
+            Console.WriteLine("Date of Sale : " + dateofsale);
+            Console.WriteLine("Total Amount " + Qty +  " * " + Price + " = "  +TotalAmount);
+
+
+        }
+
+    }
+}
